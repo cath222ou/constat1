5a6020b4a02d8ca9b6d862afb6d13e9095a6778d
cath222ou <30536406+cath222ou@users.noreply.github.com>
2017-11-10 16:59:11 -0500

    constat10-11-2017

---
+++ www/js/edition.js
@@ -0,0 +1,49 @@
+var currentRow;
+
+       
+
+		
+	// Édition de la BD
+		$('#exampleModal').modal({show:false}).
+			on('show.bs.modal', function (event) {
+				var row = $(event.relatedTarget).closest('tr');
+				var modal = $(this);
+				var dateComplete = row.find('td[data-title="b_date"]');
+				console.log(dateComplete);
+				//var anneeAA = dateComplete.substr(dateComplete.length - 2);
+            	//console.log(dateComplete);
+				modal.find('.modal-title').text('Édition du constat' +' '+ row.find('td[data-title="user_id"]').html() +"-"+ row.find('td[data-title="id"]').html());
+				modal.find('#noCivEdit').val(row.find('td[data-title="adresse"]').html());
+		});
+
+
+
+
+        // Transaction error callback
+        //
+        function errorCB(err) {
+            alert("Error processing SQL: "+err.code);
+        }
+
+        // Transaction success callback
+        //
+        function successCB() {
+            db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+            db.transaction(queryDB, errorCB);
+        }
+
+
+
+        function editRow(tx) {
+            tx.executeSql('UPDATE DEMO SET adresse ="'+document.getElementById("adresseTxt").value+
+                    '", description= "'+document.getElementById("descriptionTxt").value+ '" WHERE id = '
+                    + currentRow, [], queryDB, errorCB);
+        }
+		
+        function goEdit() {
+            db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+            db.transaction(editRow, errorCB);
+        }
+
+
+	
