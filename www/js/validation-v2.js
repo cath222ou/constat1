9de2c852fbb55e713ea674339010fbf6157e5102
richardalain83 <richard@solutionsboreal.com>
2018-03-09 08:22:58 -0500

    ajout origine cdvfile, désactive validation, renommer onglet

--- www/js/validation-v2.js
+++ www/js/validation-v2.js
@@ -1,22 +1,23 @@
 //Remplir les champs vides par null
 
 function validA() {
-    var a = $('#accordion-2');
-    var b = $('#accordion-5');
-    var c = $('#accordion-7');
-
-    a.find("input").each(function () {
-        ($(this).val() === "" ? $(this).val('null') : $(this).val())
-    });
-    b.find("textarea").each(function () {
-        ($(this).val() === "" ? $(this).val('null') : $(this).val())
-    });
-    c.find("textarea").each(function () {
-        ($(this).val() === "" ? $(this).val('null') : $(this).val())
-    });
-
-    $('#endroitTxt').val() === "" ? $('#endroitTxt').val('null') : $('#endroitTxt').val();
-    $('#descLieux').val() === "" ? $('#descLieux').val('null') : $('#descLieux').val();
+//    var a = $('#accordion-2');
+//    var b = $('#accordion-5');
+//    var c = $('#accordion-7');
+//
+//    a.find("input").each(function () {
+//        ($(this).val() === "" ? $(this).val('null') : $(this).val())
+//    });
+//    b.find("textarea").each(function () {
+//        ($(this).val() === "" ? $(this).val('null') : $(this).val())
+//    });
+//    c.find("textarea").each(function () {
+//        ($(this).val() === "" ? $(this).val('null') : $(this).val())
+//    });
+//
+//    $('#endroitTxt').val() === "" ? $('#endroitTxt').val('null') : $('#endroitTxt').val();
+//    $('#descLieux').val() === "" ? $('#descLieux').val('null') : $('#descLieux').val();
+//
 }
 
 
@@ -24,22 +25,22 @@ function validA() {
 
 function validAEdit() {
 
-    var e = $('#accordion_2');
-    var f = $('#accordion_5');
-    var g = $('#accordion_7');
-
-    e.find("input").each(function(){
-        ($(this).val() === "" ? $(this).val('null') : $(this).val())
-    });
-    f.find("input").each(function(){
-        ($(this).val() === "" ? $(this).val('null') : $(this).val())
-    });
-    g.find("input").each(function(){
-        ($(this).val() === "" ? $(this).val('null') : $(this).val())
-    });
-
-    $('#endroitTxtEdit').val() === "" ? $('#endroitTxtEdit').val('null') : $('#endroitTxtEdit').val();
-    $('#descLieuxEdit').val() === "" ? $('#descLieuxEdit').val('null') : $('#descLieuxEdit').val();
+    //var e = $('#accordion_2');
+    //var f = $('#accordion_5');
+    //var g = $('#accordion_7');
+    //
+    //e.find("input").each(function(){
+    //    ($(this).val() === "" ? $(this).val('null') : $(this).val())
+    //});
+    //f.find("input").each(function(){
+    //    ($(this).val() === "" ? $(this).val('null') : $(this).val())
+    //});
+    //g.find("input").each(function(){
+    //    ($(this).val() === "" ? $(this).val('null') : $(this).val())
+    //});
+    //
+    //$('#endroitTxtEdit').val() === "" ? $('#endroitTxtEdit').val('null') : $('#endroitTxtEdit').val();
+    //$('#descLieuxEdit').val() === "" ? $('#descLieuxEdit').val('null') : $('#descLieuxEdit').val();
 }
 
 //Validation numéro de téléphone ne doivent contenir que des chiffres
