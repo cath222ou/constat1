d68cdc856a80948d01b67e5eafec85536de15277
cath222ou <30536406+cath222ou@users.noreply.github.com>
2018-11-09 11:55:51 -0500

    uuid non mapper

--- platforms/ios/www/js/SQLite.js
+++ platforms/ios/www/js/SQLite.js
@@ -3,8 +3,8 @@
 //Variable
 var x = '';
 var y = '';
-var uuidValue = '2E9F2ADA-8992-4971-925A-C2DCDA04042A'; ///////////changerrr
-// var uuidValue;
+// var uuidValue = '2E9F2ADA-8992-4971-925A-C2DCDA04042A'; ///////////changerrr
+var uuidValue;
 $(function(){
 	$('#nouvConstat').fadeOut();
 	$('#enrVideo').fadeOut();
@@ -19,9 +19,9 @@ $(function(){
 //Cordova ready
 function onDeviceReady() {
 	//Valeur du UUID de l'appareil
-	// uuidValue = device.uuid; ////////////////////////////////////changer
+	uuidValue = device.uuid; ////////////////////////////////////changer
 	$('#uuid').html(uuidValue);
-	$('#buildVersion').html('1.2.7');
+	$('#buildVersion').html('1.2.8');
     db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
     //Création de la Table de constat
     db.transaction(populateDBConstat, errorCB, getConstatsNonSync);
